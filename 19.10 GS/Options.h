987e29ef4dc8c86a812613c51c6efd363de6e70c
CrystalFN <onbro872@gmail.com>
2025-09-26 00:10:18 -0700

    Update Options.h

--- 19.10 GS/Options.h
+++ 19.10 GS/Options.h
@@ -3,9 +3,9 @@ constexpr bool bDuos = false;
 constexpr bool bLateGame = true;
 constexpr bool bTournament = false;
 constexpr bool bCreative = false;
-constexpr bool bGameSessions = true;
+constexpr bool bGameSessions = false;
 
-constexpr bool bDev = false;
+constexpr bool bDev = true;
 
 constexpr std::string IP = "91.208.92.200"; // eu: 185.206.149.110, nac: 23.134.168.92, nae: 172.93.104.100
 constexpr std::string bRegion = "EU";
